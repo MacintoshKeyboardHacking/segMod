bb36f82c1bd241b18cfdff24e0d13cdd0086523e
MacintoshKeyboardHacking <>
2025-10-14 10:30:41 -0400

    TX bugs, add UART, WS2812 gauges

--- myBLE2/myBLE2.c
+++ myBLE2/myBLE2.c
@@ -1,7 +1,8 @@
 // myBLEv2, MIT license
 // a Segway "3 series" BLE display replacement on Arduino ESP32
 //
-// this is alpha, but the scooter drives fine and all the controls work normally
+// this is now beta.  fixed some TX bugs, added additional UART
+// added speedometer and battery gauge on 2x8 WS2812s 
 // bluetooth is not currently supported, but I've had it working in the past (myBLEv1
 // project) so it shouldn't be too bad... this release spent a lot of time laying the
 // groundwork with message fragmentation and packet validation
@@ -13,21 +14,24 @@
 #include <string.h>
 #include <stdint.h>
 
-
 #ifdef ARDUINO
 #define ESPLED sure
 
+#include <HardwareSerial.h>
 // GPIO pins to VCU UART
 #define RXD1 16
 #define TXD1 17
-
-#include <HardwareSerial.h>
 HardwareSerial VCU (1);
 
-#define NUM_LEDS 16
-#define FASTLED_LED_OVERCLOCK 1.4
+// GPIO pins to BLE UART
+#define RXD2 26
+#define TXD2 25
+HardwareSerial BLE (2);
+
 #include <FastLED.h>
+#define NUM_LEDS 16
 static CRGB leds[NUM_LEDS];
+#define FASTLED_LED_OVERCLOCK 1.4
 
 #else
 #include <unistd.h>
@@ -42,7 +46,6 @@ unsigned long millis(void) {
   return ( ts.tv_sec * 1000 + ts.tv_nsec / 1000000L );
 }
 
-
 typedef int16_t word;
 #define DEBUG output
 #endif
@@ -50,7 +53,7 @@ typedef int16_t word;
 typedef unsigned char byte;
 
 static const int maxIFB = 5;  // number of physical interfaces to route
-static const int rxBS = 256;   // serial interface block transfer size
+static const int rxBS = 256;  // serial interface block transfer size
 
 static byte ifRX[5][0x200] = { 0 }; // set to match maxIFB, per-interface RX queue
 static byte ifTX[5][0x200] = { 0 }; // a valid packet could be over 260 bytes...
@@ -64,7 +67,6 @@ static byte *ECUbuf;          // ECU malloc() as u8
 static unsigned long *ECU32;  // ECU as u32
 static unsigned long *ECUptr; // convenience pointer
 
-
 FILE *datIn;
 
 static int t, u;
@@ -73,27 +75,31 @@ static int i, j, k, l;
 static int running = 1;
 int comPort;
 
-
 static unsigned long timeNow;
 
-static byte initStr[] = {0x5a, 0xa5, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+// myBLE (proprietary) service announce
+static byte initStr[] = {0x5a, 0xa5, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff};
+
+// heartbeat PING 04>16 7a:
 static byte hb1[] = {0x5a, 0xa5, 0x00, 0x04, 0x16, 0x7a, 0x00, 0x6b, 0xff};
+
+// heartbeat PONG 04>16 7b:
 static byte hb2[] = {0x5a, 0xa5, 0x00, 0x04, 0x16, 0x7b, 0x02, 0x68, 0xff};
+
+// heartbeat stop the error beep, any read from 0x23 to 0x16 works
 static byte hb3[] = {0x5a, 0xa5, 0x00, 0x23, 0x16, 0x01, 0xfc, 0xc9, 0xfe};
-// cmdZ=b'\x5a\xa5\x00\x23\x16\x01\xfa\xcb\xfe'
 
 // hand calculated VCU subscription for speed and battery
 static byte bSub[] = {0x5a, 0xa5, 0x0c, 0x23, 0x16, 0x03, 0xfd, 0x01, 0x0a, 0x44, 0x6c, 0xe5, 0x6f, 0x10, 0x64, 0xd0, 0xf6, 0xa4, 0x8, 0xc5, 0xf9};
 
-
+// delay between heartbeat packets
 static int hbDelay = 2400;
-static int hbInactive = 60000;
 static int hbPhase = 0;
 
 static int myPower = 1;
 
 #ifdef ESPLED
-#include "hal/gpio_ll.h" // GPIO register functions
+#include "hal/gpio_ll.h"  // GPIO register functions
 
 // gamma correct linear light
 static word table[] = {0x0000, 0x0003, 0x0008, 0x000f, 0x0018, 0x0023, 0x0030, 0x003f, 0x0050, 0x0063, 0x0078, 0x008f, 0x00a8, 0x00c3, 0x00e0, 0x00ff, 0x0120, 0x0143, 0x0168, 0x018f, 0x01b8, 0x01e3, 0x0210, 0x023f, 0x0270, 0x02a3, 0x02d8, 0x030f, 0x0348, 0x0383, 0x03c0, 0x03ff, 0x0440, 0x0483, 0x04c8, 0x050f, 0x0558, 0x05a3, 0x05f0, 0x063f, 0x0690, 0x06e3, 0x0738, 0x078f, 0x07e8, 0x0843, 0x08a0, 0x08ff, 0x0960, 0x09c3, 0x0a28, 0x0a8f, 0x0af8, 0x0b63, 0x0bd0, 0x0c3f, 0x0cb0, 0x0d23, 0x0d98, 0x0e0f, 0x0e88, 0x0f03, 0x0f80, 0x0fff, 0x1080, 0x1103, 0x1188, 0x120f, 0x1298, 0x1323, 0x13b0, 0x143f, 0x14d0, 0x1563, 0x15f8, 0x168f, 0x1728, 0x17c3, 0x1860, 0x18ff, 0x19a0, 0x1a43, 0x1ae8, 0x1b8f, 0x1c38, 0x1ce3, 0x1d90, 0x1e3f, 0x1ef0, 0x1fa3, 0x2058, 0x210f, 0x21c8, 0x2283, 0x2340, 0x23ff, 0x24c0, 0x2583, 0x2648, 0x270f, 0x27d8, 0x28a3, 0x2970, 0x2a3f, 0x2b10, 0x2be3, 0x2cb8, 0x2d8f, 0x2e68, 0x2f43, 0x3020, 0x30ff, 0x31e0, 0x32c3, 0x33a8, 0x348f, 0x3578, 0x3663, 0x3750, 0x383f, 0x3930, 0x3a23, 0x3b18, 0x3c0f, 0x3d08, 0x3e03, 0x3f00, 0x3fff, 0x4100, 0x4203, 0x4308, 0x440f, 0x4518, 0x4623, 0x4730, 0x483f, 0x4950, 0x4a63, 0x4b78, 0x4c8f, 0x4da8, 0x4ec3, 0x4fe0, 0x50ff, 0x5220, 0x5343, 0x5468, 0x558f, 0x56b8, 0x57e3, 0x5910, 0x5a3f, 0x5b70, 0x5ca3, 0x5dd8, 0x5f0f, 0x6048, 0x6183, 0x62c0, 0x63ff, 0x6540, 0x6683, 0x67c8, 0x690f, 0x6a58, 0x6ba3, 0x6cf0, 0x6e3f, 0x6f90, 0x70e3, 0x7238, 0x738f, 0x74e8, 0x7643, 0x77a0, 0x78ff, 0x7a60, 0x7bc3, 0x7d28, 0x7e8f, 0x7ff8, 0x8163, 0x82d0, 0x843f, 0x85b0, 0x8723, 0x8898, 0x8a0f, 0x8b88, 0x8d03, 0x8e80, 0x8fff, 0x9180, 0x9303, 0x9488, 0x960f, 0x9798, 0x9923, 0x9ab0, 0x9c3f, 0x9dd0, 0x9f63, 0xa0f8, 0xa28f, 0xa428, 0xa5c3, 0xa760, 0xa8ff, 0xaaa0, 0xac43, 0xade8, 0xaf8f, 0xb138, 0xb2e3, 0xb490, 0xb63f, 0xb7f0, 0xb9a3, 0xbb58, 0xbd0f, 0xbec8, 0xc083, 0xc240, 0xc3ff, 0xc5c0, 0xc783, 0xc948, 0xcb0f, 0xccd8, 0xcea3, 0xd070, 0xd23f, 0xd410, 0xd5e3, 0xd7b8, 0xd98f, 0xdb68, 0xdd43, 0xdf20, 0xe0ff, 0xe2e0, 0xe4c3, 0xe6a8, 0xe88f, 0xea78, 0xec63, 0xee50, 0xf03f, 0xf230, 0xf423, 0xf618, 0xf80f, 0xfa08, 0xfc03, 0xfe00, 0xffff};
@@ -113,8 +119,7 @@ static unsigned long timeGo = 0;
 static unsigned long timeLast = 0;
 static int goPhase = 0;
 
-void
-setup () {
+void setup () {
 #ifdef DEBUG
   printf ("setup\n");
 #endif
@@ -127,7 +132,10 @@ setup () {
   VCU.setTimeout (0);
   VCU.begin (115200, SERIAL_8N1, RXD1, TXD1);
 
-  while (!Serial || !VCU);
+  BLE.setTimeout (0);
+  BLE.begin (115200, SERIAL_8N1, RXD2, TXD2);
+
+  while (!Serial || !VCU || !BLE);
   Serial.write (initStr, sizeof (initStr));
 
   FastLED.addLeds<WS2812, 23, GRB>(leds, NUM_LEDS);
@@ -139,7 +147,6 @@ setup () {
   write(comPort, initStr, sizeof (initStr));
 #endif
 
-
   // initialize ECU buffer
   ECUbuf = (byte *) malloc(ECUlen);
   ECU32 = (unsigned long *) ECUbuf;
@@ -147,7 +154,6 @@ setup () {
   for (int u = 0; u < ECUlen; u++) {
     ECUbuf[u] = 0x0;
   }
-
 
 #ifdef ESPLED
   ledcAttachChannel(LRpin, P1freq, 16, P1chan);
@@ -169,10 +175,7 @@ setup () {
 }
 
 
-void
-loop ()
-{
-
+void loop () {
   // Take care of the heartbeat functionality
   if (millis() - timeNow > hbDelay) {
     if (myPower) {
@@ -216,8 +219,6 @@ loop ()
   }
 
 
-
-
   static byte RXbuf[0x200];
   static byte TXpkt[0x200];
   static int TXlen = 0;
@@ -225,7 +226,7 @@ loop ()
   // per interface
   for (int ifb = 0; ifb < maxIFB; ifb++) {
 
-    // first we transmi
+    // first we transmit
     TXlen = ifTXlen[ifb];
     if (TXlen) {
       if (TXlen > rxBS) {
@@ -243,16 +244,15 @@ loop ()
       memcpy (&ifTX[ifb][0], &ifTX[ifb][TXlen], TXremaining);
       ifTXlen[ifb] -= TXlen;
 
-
-
-
       // static interface routing for now: ie: only transmit to VCU or USB
-      if (myPower) {
+      if (myPower || 1) { // temp override
 #ifdef ARDUINO
         if (ifb == 1) {
           VCU.write (ifTX[ifb], TXlen);
         } else if (ifb == 0) {
           Serial.write (ifTX[ifb], TXlen);
+        } else if (ifb == 2) {
+          BLE.write (ifTX[ifb], TXlen);
         }
 #else
         if (ifb == 1) {
@@ -261,7 +261,6 @@ loop ()
 #endif
       }
     }
-
 
 
     // Receive <= BS bytes from different interfaces
@@ -280,10 +279,14 @@ loop ()
 #ifdef ARDUINO
         rxBytes = VCU.readBytes(RXbuf, rxBS);
 #else
-        rxBytes = read(comPort, RXbuf, rxBS );
+        rxBytes = read(comPort, RXbuf, rxBS);
 #endif
         break;
-      case 2:
+      case 2: // ESP32>BLE alternate
+#ifdef ARDUINO
+        rxBytes = BLE.readBytes(RXbuf, rxBS);
+#endif
+        break;
       case 3:
         break;
       case 4:
@@ -331,7 +334,6 @@ loop ()
 #ifdef DEBUG
               printf ("RX%d:", ifb);
 #endif
-
               int doXmit = 1;
 
               // copy good packet to temp TX buffer
@@ -346,7 +348,7 @@ loop ()
               printf ("\n");
 #endif
 
-              int wr_ack = 0;
+              int wr_ack = 0; // generate write acknowledgement hack
 
               byte pLEN = TXpkt[2];
               byte pSRC = TXpkt[3];
@@ -394,20 +396,16 @@ loop ()
                 }
 
                 // these flags
-                int ECU_read = (2 << 30);   // had read
-                int ECU_write = (2 << 29);  // had write
-                int ECU_grpChange = (2 << 28);  // did update
-                int ECU_valChange = (2 << 27);  // did change
-                int ECU_error = (2 << 26);  // had error
-                int ECU_bglow = (2 << 25);  // low priority bg reading
-                int ECU_noRemap = (2 << 23);// abort processing
-
-                //                ECU32[0x9fC] = ECU_noRemap;
-                //                ECU32[0x7f8] = ECU_noRemap;
+                int ECU_read = (1 << 31);   // had read
+                int ECU_write = (1 << 30);  // had write
+                int ECU_grpChange = (1 << 29);  // did update
+                int ECU_valChange = (1 << 28);  // did change
+                int ECU_error = (1 << 27);  // had error
+                int ECU_bglow = (1 << 26);  // low priority bg reading
+                int ECU_noRemap = (1 << 24);// abort processing
 
                 int pkt_writing = 0;
                 int grpChange = 0;
-
 
                 // update ECUmap based on variable access
                 if (dECU) {
@@ -415,8 +413,7 @@ loop ()
                   byte grpFX = (*ECUptr >> 16);
                   byte grpMod = (*ECUptr >> 8);
                   grpFX = 0;  // functionality not ready
-                  //                  if ( (*ECUptr && ECU_noRemap)>0 )
-                  //      long test=(*ECUptr & ECU_noRemap);
+
                   if ( (*ECUptr & ECU_noRemap)) {
 #ifdef DEBUG
                     printf("NOREMAP %08x %08x %08x\n", *ECUptr, ECU_noRemap, (*ECUptr & ECU_noRemap));
@@ -499,7 +496,6 @@ loop ()
                             }
                             break;
                         }
-
                       }
                       // replace value in new buffer
                       *ECUptr = (newFlags | (modifier << 8) | newValue);
@@ -513,7 +509,6 @@ loop ()
                     }
                   }
                 }
-
                 /*
                   // this is per the scooter hacking PDF
                             if (wr_ack) {
@@ -526,6 +521,7 @@ loop ()
                 */
 
                 // this is per sniffing
+                // BUG rewriting the buffer this way means the real write packet doesn't get forwarded
                 if (wr_ack) {
                   pLEN = 2;
                   TXpkt[3] = pDST;
@@ -535,7 +531,6 @@ loop ()
                   TXpkt[7] = 1; // write success
                   TXpkt[8] = 0; // write success
                 }
-
 
                 // recalculate packet checksum
                 int cksum = pLEN;
@@ -547,38 +542,41 @@ loop ()
                 cksum ^= 0xffff;
                 TXpkt[pLEN + 7] = (cksum & 0xff);
                 TXpkt[pLEN + 8] = (cksum >> 8);
+              } // "if ECU"
 
-
-                if (doXmit) {
+              if (doXmit) {
 #ifdef DEBUG
-                  printf("PKT:");
-                  for (int v = 0; v < TXlen; v++) {
-                    printf(" %02x", TXpkt[v]);
-                  }
-                  printf("\n");
-#endif
-
-                  // just "test routing" for now
-                  if ((ifb == 0) || (wr_ack)) {   // hardcode write ack
-                    memcpy (&ifTX[1][ifTXlen[1]],
-                            &ifRX[ifb][idx], (len + 9));
-                    ifTXlen[1] += (len + 9);
-                    wr_ack = 0;
-                  } else if (ifb == 1) {
-                    memcpy (&ifTX[0][ifTXlen[0]],
-                            &ifRX[ifb][idx], (len + 9));
-                    ifTXlen[0] += (len + 9);
-                  }
-#ifdef DEBUG
-                  usleep(1000);
-#endif
+                printf("PKT:");
+                for (int v = 0; v < TXlen; v++) {
+                  printf(" %02x", TXpkt[v]);
                 }
+                printf("\n");
+#endif
+                // basic static routing for now
+                if ((ifb == 0) || (ifb == 2) || (wr_ack)) {   // hardcode write ack BUG
+                  memcpy (&ifTX[1][ifTXlen[1]],
+                          &ifRX[ifb][idx], (len + 9));
+                  ifTXlen[1] += (len + 9);
+                  wr_ack = 0;
+                } else if (ifb == 1) {
+                  memcpy (&ifTX[0][ifTXlen[0]],
+                          &ifRX[ifb][idx], (len + 9));
+                  ifTXlen[0] += (len + 9);
+
+                  memcpy (&ifTX[2][ifTXlen[2]],
+                          &ifRX[ifb][idx], (len + 9));
+                  ifTXlen[2] += (len + 9);
+                }
+#ifdef DEBUG
+                usleep(1000);
+#endif
               }
+
             }
           }
           // move next packet to start buffer
           int remaining = (sizeof (RXbuf) - idx);
-          //BUG - when bad characters are present, this copies them; they should be dropped.
+          //BUG - when bad characters are present at the start, this copies them; they should be dropped.
           memcpy (&ifRX[ifb][0], &ifRX[ifb][idx], remaining);
           ifRXlen[ifb] -= idx;
 
@@ -589,13 +587,13 @@ loop ()
         }   // just finished, "yes it's a packet"
       }
     }
+
     // BUG hardcoded offsets are bad
-
-
     int mySpeed = ((ECU32[(0x200 * 5) + (0xfe * 2) + 1 ] & 0xff ) ); //23:fe>>8
     int myBatt = (ECU32[(0x200 * 5) + (0xff * 2) + 1 ] ) & 0xff; //23:ff>>8
 
-    int testPower = ((ECU32[(0x200 * 3) + (0x51 * 2)  ] & 0xffff ) );
+    //    int testPower = ((ECU32[(0x200 * 3) + (0x51 * 2)  ] & 0xffff ) );  // 04:51, actually means "unlocked"
+    int testPower = ((ECU32[(0x200 * 5) + (0x25 * 2)  ] & 0xffff ) ); // 23:25, screen on
     if ((testPower == 0) && (myPower > 0)) {
       myPower = 0;
 #ifdef ESPLED
@@ -606,8 +604,10 @@ loop ()
     }
 
     static int myTopSpd = 0;
+    static int myTopDly = 0;
     if (mySpeed > myTopSpd) {
       myTopSpd = mySpeed;
+      myTopDly = 1000;
     }
     static int mySpeedAvg;
     mySpeedAvg *= 7;
@@ -646,9 +646,6 @@ loop ()
       leds[15 - l].setRGB(c2, c1, 0);
     }
 
-
-    //    leds[0].setRGB( mySpeed, 3, 1);
-    //                  ECUptr = &ECU32[(0x200 * dECU) + (pADR * 2)];
     if (ledDly) {
       ledDly--;
     } else {
@@ -656,26 +653,26 @@ loop ()
       ledBri++;
       if (ledBri > 255) {
         ledBri = 0;
-        if (myTopSpd) {
-          myTopSpd--;
-        }
       }
       ledcWrite (LGpin, table[ledBri]);
       if (myPower) {
         FastLED.show();
       }
-
+      if (myTopDly) {
+        myTopDly--;
+      } else {
+        if (myTopSpd) {
+          myTopSpd--;
+        }
+      }
     }
 #endif
-
   }
-
 }
 
+
 #ifndef ARDUINO
-int
-main ()
-{
+int main () {
   setup ();
   while (running)
   {
